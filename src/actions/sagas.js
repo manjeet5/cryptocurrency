9444db791f011c24d0d6b4dbfc6dbc709ff50585
manjeet5 <manjeet175@gmail.com>
2020-05-19 18:19:33 -0400

    updated the saga file to use axios library instead of mockdata

--- src/actions/sagas.js
+++ src/actions/sagas.js
@@ -16,13 +16,22 @@ import {
   addCryptoToDropdown,
 } from "./actionCreators";
 import axios from "axios";
-import { cryptoPricesApi, cryptoCurrenciesApi } from "../mock/mockEndpoints";
+// import { cryptoPricesApi, cryptoCurrenciesApi } from "../mock/mockEndpoints";
+
+const BASE_URL = "https://www.stackadapt.com/coinmarketcap";
 
 const getPriceDetailUrl = (cryptoIds) => {
-  return `https://www.stackadapt.com/coinmarketcap/quotes?id=${cryptoIds.toString()}`;
+  return `/quotes?id=${cryptoIds.toString()}`;
 };
 
-function* addCryptToTable(action) {
+const getUrlOptions = (extendUrl) => {
+  return {
+    method: "get",
+    url: `${BASE_URL}${extendUrl}`,
+  };
+};
+
+function* addCryptoToTable(action) {
   const { payload } = action;
   yield call(fetchCryptoCurrenciesPrices, action);
   yield put(removeCryptoFromDropdown(payload));
@@ -33,18 +42,16 @@ function* removeCryptoFromTableSaga(action) {
   yield put(removeCryptoFromTableList(payload));
   yield put(addCryptoToDropdown(payload));
 }
-function* fetchCryptoCurrenciesPrices(action, inititalTableRenderList) {
-  console.log("cryptoCurrencies", inititalTableRenderList);
 
+function* fetchCryptoCurrenciesPrices(action, inititalTableRenderList) {
   try {
     const { payload } = action;
     const cryptoList = inititalTableRenderList || [payload.id];
     if (cryptoList.length) {
-      const urlOptions = {
-        method: "get",
-        url: getPriceDetailUrl(cryptoList),
-      };
-      const cryptoCurrenciesPrices = yield call(cryptoPricesApi, urlOptions);
+      const cryptoCurrenciesPrices = yield call(
+        axios,
+        getUrlOptions(getPriceDetailUrl(cryptoList))
+      );
       const { status, data } = cryptoCurrenciesPrices.data;
       if (status.error_message) {
         yield put(setError(status.error_message));
@@ -58,13 +65,10 @@ function* fetchCryptoCurrenciesPrices(action, inititalTableRenderList) {
     yield put(setError(error.message));
   }
 }
+
 function* fetchCryptoCurrencies(action) {
   try {
-    const urlOptions = {
-      method: "get",
-      url: "https://www.stackadapt.com/coinmarketcap/map",
-    };
-    const cryptoCurrenciesList = yield call(cryptoCurrenciesApi, urlOptions);
+    const cryptoCurrenciesList = yield call(axios, getUrlOptions("/map"));
     const { status, data } = cryptoCurrenciesList.data;
     if (status.error_message) {
       yield put(setError(status.error_message));
@@ -82,7 +86,7 @@ function* fetchCryptoCurrencies(action) {
 
 function* mySaga() {
   yield takeLatest(CRYPTO_LIST_FETCH_REQUEST, fetchCryptoCurrencies);
-  yield takeEvery(ADD_CRYPTO_TO_TABLE, addCryptToTable);
+  yield takeEvery(ADD_CRYPTO_TO_TABLE, addCryptoToTable);
   yield takeEvery(REMOVE_CRYPTO_FROM_TABLE, removeCryptoFromTableSaga);
 }
 

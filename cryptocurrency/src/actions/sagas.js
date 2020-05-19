56d222373cccaa7bc43dd0409f3c1fb919056fdb
manjeet5 <manjeet175@gmail.com>
2020-05-19 11:15:14 -0400

    added remote endpoint and updated mockdata data size

--- cryptocurrency/src/actions/sagas.js
+++ cryptocurrency/src/actions/sagas.js
@@ -1,22 +1,31 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import _ from "lodash";
+// import axios from "axios";
 import {
     ADD_CRYPTO_TO_TABLE,
     CRYPTO_LIST_FETCH_REQUEST
 } from "./actionTypes";
 import {setError, setCryptoList, setCryptoPrices} from "./actionCreators";
-import _ from "lodash";
+import axios from "axios";
 import {cryptoPricesApi, cryptoCurrenciesApi} from "../mock/mockEndpoints";
 
 const getPriceDetailUrl = (cryptoIds) => {
     return `https://www.stackadapt.com/coinmarketcap/quotes?id=${cryptoIds.toString()}`;
 }
 function* fetchCryptoCurrenciesPrices(action, cryptoCurrencies) {
+    console.log("cryptoCurrencies", cryptoCurrencies);
+
     try {
         const {payload} = action;
         const cryptoList = payload.initialLoad ? _.map(cryptoCurrencies, "id").slice(0,5) : [payload.id]
         if(cryptoList.length) {
-            const priceDetailUrl = getPriceDetailUrl(cryptoList);
-            const  {status, data} = yield call(cryptoPricesApi, priceDetailUrl);
+            // const priceDetailUrl = getPriceDetailUrl(cryptoList);
+            const urlOptions = {
+                method: "get", url: getPriceDetailUrl(cryptoList)
+            }
+            console.log("urlOptions", urlOptions);
+            const  cryptoCurrenciesPrices = yield call(cryptoPricesApi, urlOptions);
+            const  {status, data} = cryptoCurrenciesPrices.data;
             if(status.error_message) {
                 yield put(setError(status.error_message));
             } else {
@@ -32,13 +41,20 @@ function* fetchCryptoCurrenciesPrices(action, cryptoCurrencies) {
 }
 function* fetchCryptoCurrencies(action) {
     try {
-        const {status, data} = yield call(cryptoCurrenciesApi);
+        const urlOptions = {
+            method: "get", url: "https://www.stackadapt.com/coinmarketcap/map"
+        }
+        const cryptoCurrenciesList = yield call(cryptoCurrenciesApi, urlOptions);
+        const  {status, data} = cryptoCurrenciesList.data;
         if(status.error_message) {
             yield put(setError(status.error_message));
+        } else {
+            yield put(setCryptoList(data));
+
+            yield call(fetchCryptoCurrenciesPrices, action, data)
         }
-        yield put(setCryptoList(data));
         
-        yield call(fetchCryptoCurrenciesPrices, action, data)
+       
     } catch (error) {
         yield put(setError(error.message));
     }

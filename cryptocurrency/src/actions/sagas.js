6ecb1a8f3200d7f69ed19ebb432ce0cf022879d6
manjeet5 <manjeet175@gmail.com>
2020-05-19 14:14:43 -0400

    minor changes

--- cryptocurrency/src/actions/sagas.js
+++ cryptocurrency/src/actions/sagas.js
@@ -1,84 +1,89 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
 import _ from "lodash";
 // import axios from "axios";
 import {
-    ADD_CRYPTO_TO_TABLE,
-    CRYPTO_LIST_FETCH_REQUEST,
-    REMOVE_CRYPTO_FROM_TABLE
+  ADD_CRYPTO_TO_TABLE,
+  CRYPTO_LIST_FETCH_REQUEST,
+  REMOVE_CRYPTO_FROM_TABLE,
 } from "./actionTypes";
-import {setError, setCryptoList, setCryptoPrices, setDropdown, removeCryptoFromDropdown, removeCryptoFromTableList, addCryptoToDropdown} from "./actionCreators";
+import {
+  setError,
+  setCryptoList,
+  setCryptoPrices,
+  setDropdown,
+  removeCryptoFromDropdown,
+  removeCryptoFromTableList,
+  addCryptoToDropdown,
+} from "./actionCreators";
 import axios from "axios";
-import {cryptoPricesApi, cryptoCurrenciesApi} from "../mock/mockEndpoints";
+import { cryptoPricesApi, cryptoCurrenciesApi } from "../mock/mockEndpoints";
 
 const getPriceDetailUrl = (cryptoIds) => {
-    return `https://www.stackadapt.com/coinmarketcap/quotes?id=${cryptoIds.toString()}`;
-}
+  return `https://www.stackadapt.com/coinmarketcap/quotes?id=${cryptoIds.toString()}`;
+};
 
 function* addCryptToTable(action) {
-    const {payload} = action;
-    yield call(fetchCryptoCurrenciesPrices, action)
-    yield put(removeCryptoFromDropdown(payload));
+  const { payload } = action;
+  yield call(fetchCryptoCurrenciesPrices, action);
+  yield put(removeCryptoFromDropdown(payload));
 }
 
 function* removeCryptoFromTableSaga(action) {
-    const {payload} = action;
-    yield put(removeCryptoFromTableList(payload));
-    yield put(addCryptoToDropdown(payload));
+  const { payload } = action;
+  yield put(removeCryptoFromTableList(payload));
+  yield put(addCryptoToDropdown(payload));
 }
 function* fetchCryptoCurrenciesPrices(action, inititalTableRenderList) {
-    console.log("cryptoCurrencies", inititalTableRenderList);
+  console.log("cryptoCurrencies", inititalTableRenderList);
 
-    try {
-        const {payload} = action;
-        const cryptoList = inititalTableRenderList || [payload.id]
-        if(cryptoList.length) {
-            // const priceDetailUrl = getPriceDetailUrl(cryptoList);
-            const urlOptions = {
-                method: "get", url: getPriceDetailUrl(cryptoList)
-            }
-            console.log("urlOptions", urlOptions);
-            const  cryptoCurrenciesPrices = yield call(cryptoPricesApi, urlOptions);
-            const  {status, data} = cryptoCurrenciesPrices.data;
-            if(status.error_message) {
-                yield put(setError(status.error_message));
-            } else {
-                yield put(setCryptoPrices(data));
-            }
-        } else {
-            yield put(setError("no crypto id provided"));
-        }
-    } catch(error) {
-        yield put(setError(error.message));
+  try {
+    const { payload } = action;
+    const cryptoList = inititalTableRenderList || [payload.id];
+    if (cryptoList.length) {
+      const urlOptions = {
+        method: "get",
+        url: getPriceDetailUrl(cryptoList),
+      };
+      const cryptoCurrenciesPrices = yield call(cryptoPricesApi, urlOptions);
+      const { status, data } = cryptoCurrenciesPrices.data;
+      if (status.error_message) {
+        yield put(setError(status.error_message));
+      } else {
+        yield put(setCryptoPrices(data));
+      }
+    } else {
+      yield put(setError("no crypto id provided"));
     }
-    
+  } catch (error) {
+    yield put(setError(error.message));
+  }
 }
 function* fetchCryptoCurrencies(action) {
-    try {
-        const urlOptions = {
-            method: "get", url: "https://www.stackadapt.com/coinmarketcap/map"
-        }
-        const cryptoCurrenciesList = yield call(cryptoCurrenciesApi, urlOptions);
-        const  {status, data} = cryptoCurrenciesList.data;
-        if(status.error_message) {
-            yield put(setError(status.error_message));
-        } else {
-            yield put(setCryptoList(data));
-            const inititalTableRenderList = _.map(data, "id").slice(0,5);
-            yield put(setDropdown({data, inititalTableRenderList}));
+  try {
+    const urlOptions = {
+      method: "get",
+      url: "https://www.stackadapt.com/coinmarketcap/map",
+    };
+    const cryptoCurrenciesList = yield call(cryptoCurrenciesApi, urlOptions);
+    const { status, data } = cryptoCurrenciesList.data;
+    if (status.error_message) {
+      yield put(setError(status.error_message));
+    } else {
+      yield put(setCryptoList(data));
+      const inititalTableRenderList = _.map(data, "id").slice(0, 5);
+      yield put(setDropdown({ data, inititalTableRenderList }));
 
-            yield call(fetchCryptoCurrenciesPrices, action, inititalTableRenderList)
-        }
-        
-       
-    } catch (error) {
-        yield put(setError(error.message));
+      yield call(fetchCryptoCurrenciesPrices, action, inititalTableRenderList);
     }
- }
-
- function* mySaga() {
-    yield takeLatest(CRYPTO_LIST_FETCH_REQUEST, fetchCryptoCurrencies);
-    yield takeEvery(ADD_CRYPTO_TO_TABLE, addCryptToTable);
-    yield takeEvery(REMOVE_CRYPTO_FROM_TABLE, removeCryptoFromTableSaga)
+  } catch (error) {
+    yield put(setError(error.message));
   }
+}
 
- export default mySaga;
+function* mySaga() {
+  yield takeLatest(CRYPTO_LIST_FETCH_REQUEST, fetchCryptoCurrencies);
+  yield takeEvery(ADD_CRYPTO_TO_TABLE, addCryptToTable);
+  yield takeEvery(REMOVE_CRYPTO_FROM_TABLE, removeCryptoFromTableSaga);
+}
+
+export default mySaga;
--- cryptocurrency/src/components/CurrencyDetailsTable.jsx
+++ cryptocurrency/src/components/CurrencyDetailsTable.jsx
@@ -1,54 +1,59 @@
 import React from "react";
-import { Table, Button } from 'antd';
-import {connect} from "react-redux";
-import {removeCryptoFromTable} from "../actions/actionCreators";
-import {getTableListDetails} from "../selectors/selectors";
+import { Table, Button } from "antd";
+import { connect } from "react-redux";
+import { removeCryptoFromTable } from "../actions/actionCreators";
+import { getTableListDetails } from "../selectors/selectors";
 
-const mapDispatchToProps = (dispatch) => {
-    return ({
-        // delete: removeCurrencyFromTable(dispatch)
-        dispatch
-    })
-}
 const CurrencyDetailsTable = (props) => {
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Symbol',
-          dataIndex: 'symbol',
-          key: 'symbol'
-        },
-        {
-          title: 'CMC Rank',
-          dataIndex: 'cmc_rank',
-          key: 'cmc_rank',
-          sorter: {
-            compare: (a, b) => a.cmc_rank - b.cmc_rank,
-          }
-        },
-        {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'price',
-            sorter: {
-              compare: (a, b) => a.price - b.price,
-            }
-          },
-       
-        {
-          title: 'Action',
-          key: 'action',
-          render: (record) => (
-            <Button onClick={() => props.dispatch(removeCryptoFromTable(record.id))} disabled={props.tableList.length < 2}>Delete</Button>
-          ),
-        },
-      ];
-      
-      return <Table columns={columns} dataSource={props.tableList} pagination={{ hideOnSinglePage: true}}/>;
-}
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Symbol",
+      dataIndex: "symbol",
+      key: "symbol",
+    },
+    {
+      title: "CMC Rank",
+      dataIndex: "cmc_rank",
+      key: "cmc_rank",
+      sorter: {
+        compare: (a, b) => a.cmc_rank - b.cmc_rank,
+      },
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+      sorter: {
+        compare: (a, b) => a.price - b.price,
+      },
+    },
 
-export default connect(getTableListDetails, mapDispatchToProps)(CurrencyDetailsTable);
+    {
+      title: "Action",
+      key: "action",
+      render: (record) => (
+        <Button
+          onClick={() => props.dispatch(removeCryptoFromTable(record.id))}
+          disabled={props.tableList.length < 2}
+        >
+          Delete
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={props.tableList}
+      pagination={{ hideOnSinglePage: true }}
+    />
+  );
+};
+
+export default connect(getTableListDetails)(CurrencyDetailsTable);

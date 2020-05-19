5f5e3d06a8f44f04c9d456a6982f1b7bc6a3f5f3
manjeet5 <manjeet175@gmail.com>
2020-05-19 18:04:59 -0400

    fixed logic such that dropdown button would be disabled when number of rows is greater than or equal to 10

--- src/components/DropdownList.jsx
+++ src/components/DropdownList.jsx
@@ -1,41 +1,53 @@
 import React from "react";
-import { Menu, Dropdown, Button } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import {connect} from "react-redux";
-import {getDropdownListDetails} from "../selectors/selectors";
-import {addCurrencyToTable} from "../actions/actionCreators";
+import { Menu, Dropdown, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
+import { getDropdownListDetails } from "../selectors/selectors";
+import { addCurrencyToTable } from "../actions/actionCreators";
 
 const { SubMenu } = Menu;
 const mapDispatchToProps = (dispatch) => {
-    return {
-        add: addCurrencyToTable(dispatch)
-    }
-}
+  return {
+    add: addCurrencyToTable(dispatch),
+  };
+};
 const DropDownList = (props) => {
-    const {dropdown, add, tableListIds} = props;
+  const { dropdown, add, tableListIds } = props;
 
-    const renderMenuItems = (list) => {
-        return list.map(currency => {
-            return <Menu.Item key={currency.id}>{currency.name}</Menu.Item>
-        })
-    }
-    const renderSubMenus = (category) => {
-        return <SubMenu key={category} trigger={['click']} title={category}>
-            {renderMenuItems(dropdown[category])}
-        </SubMenu>
-    }
-    const menu = (
-        <Menu onClick={(event)=>add(event.key)}>
-            {Object.keys(dropdown).map(category => renderSubMenus(category))}
-        </Menu>
-      );
+  const renderMenuItems = (list) => {
+    return list.map((currency) => {
+      return <Menu.Item key={currency.id}>{currency.name}</Menu.Item>;
+    });
+  };
+  const renderSubMenus = (category) => {
     return (
-        <div className="drop-down-button">
-            <Dropdown overlay={menu} trigger={['click']}  placement="bottomLeft" disabled={tableListIds.length > 10}>
-                <Button  type="primary" >CryptoPrice <DownOutlined /></Button>
-            </Dropdown>
-        </div>
-    )
-}
+      <SubMenu key={category} trigger={["click"]} title={category}>
+        {renderMenuItems(dropdown[category])}
+      </SubMenu>
+    );
+  };
+  const menu = (
+    <Menu onClick={(event) => add(event.key)}>
+      {Object.keys(dropdown).map((category) => renderSubMenus(category))}
+    </Menu>
+  );
+  return (
+    <div className="drop-down-button">
+      <Dropdown
+        overlay={menu}
+        trigger={["click"]}
+        placement="bottomLeft"
+        disabled={tableListIds.length >= 10}
+      >
+        <Button type="primary">
+          CryptoPrice <DownOutlined />
+        </Button>
+      </Dropdown>
+    </div>
+  );
+};
 
-export default connect(getDropdownListDetails, mapDispatchToProps)(DropDownList);    
+export default connect(
+  getDropdownListDetails,
+  mapDispatchToProps
+)(DropDownList);
